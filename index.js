1f37ad7fad99daf9c5be76aa571a8d8772f26ccd
Cody W <cody@codywhitby.net>
2016-06-13 00:12:06 -0400

    better comment and a blank line

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var dash_button = require('node-dash-button');
-var dash = dash_button("00:7f:28:25:fc:eb"); //address from step above
+
+var dash = dash_button("00:7f:28:25:fc:eb"); //mac address of dash button
 dash.on("detected", function (){
     console.log("omg found");
 });
